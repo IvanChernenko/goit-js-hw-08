86325ae4a42670af236f316e9de483ffe8ce4f33
IvanChernenko <ivanchernenko92@gmail.com>
2022-06-23 02:10:54 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from "lodash";
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+}
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextAreaInput, 500));
+
+populateTextArea();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    refs.form.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    console.log(localStorage.getItem(LOCALSTORAGE_KEY));
+    
+}
+
+function onTextAreaInput(e) {
+    const message = {
+        email: e.target.value,
+        message: e.target.value,
+    };
+
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(message));
+}
+
+function populateTextArea() {
+    try {
+        const savedMessage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || ``;
+        if (savedMessage) {
+            const getMessage = savedMessage.message;
+            const getMail = savedMessage.email;
+            refs.textarea.value = getMessage;
+            refs.input.value = getMail;
+        }
+    }
+    catch {
+        console.log('error');
+    }
+}
